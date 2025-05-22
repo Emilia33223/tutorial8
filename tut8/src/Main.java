e88ce97297931b41fc8d293656d53d23e62e1b1b
Emilia33223 <s33223@pjwstk.edu.pl>
2025-05-22 19:47:26 +0200

    added lines

--- tut8/src/Main.java
+++ tut8/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Animal lion = new Lion("Simba", 1, "male");
+        Animal penguin = new Penguin("Gacek", 4, true);
+        Animal crocodile = new Crocodile("Nile", 12, "green");
+        ((Lion)lion).hunt();
+        lion.displayInformation();
+        penguin.displayInformation();
+        crocodile.displayInformation();
     }
 }
